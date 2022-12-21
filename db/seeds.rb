23f34e025f328f0ef111bddbd62ee70064d4567a
Prescott <carlsonprescott@gmail.com>
2022-12-21 12:06:55 -0800

    demo user added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+user1 = User.create!(
+  first_name: 'Demo', last_name: 'User', email: 'demo_user@mail.com', password: 'password123'
+)
