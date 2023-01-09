e9581aa8c3bc4d30605070563df9fbd715d3f0a0
Prescott <carlsonprescott@gmail.com>
2023-01-09 15:02:07 -0800

    fixed login bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,13 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
   validates :password_digest, :session_token, presence: true
-  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :password, length: { minimum: 6 }, allow_nil: true 
 
   after_initialize :ensure_session_token
 
   def self.find_by_credentials(email, password)
     user = User.find_by(email: email)
     return nil unless user && user.is_password?(password)
-
     user
   end
 
