67da2f6f1c7afad0482d2bb5ea2eec44c7c0b79f
Prescott <carlsonprescott@gmail.com>
2023-01-11 14:14:52 -0800

    homepage component created

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,12 +1,6 @@
-// import {
-//   RECEIVE_SESSION_ERRORS,
-//   RECEIVE_CURRENT_USER,
-//   CLEAR_SESSION_ERRORS
-// } from "../actions/session_actions";
-
 import {
   RECEIVE_SESSION_ERRORS,
-  RECEIVE_CURRENT_USER
+  RECEIVE_CURRENT_USER,
 } from "../actions/session_actions";
 
 export default (state = [], action) => {
@@ -20,23 +14,3 @@ export default (state = [], action) => {
       return state;
   }
 };
-
-
-// const SessionErrorsReducer = (state = {}, action) => {
-//   Object.freeze(state);
-//   let nextState = Object.assign({}, state);
-//   switch (action.type) {
-//     case RECEIVE_CURRENT_USER:
-//       nextState = [];
-//       return nextState;
-//     case RECEIVE_SESSION_ERRORS:
-//       nextState = action.errors.responseJSON;
-//       return nextState;
-//     case CLEAR_SESSION_ERRORS:
-//       return [];
-//     default:
-//       return state;
-//   }
-// }
-
-// export default SessionErrorsReducer
