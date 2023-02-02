b7b1707817f458c56fbec84d6b745fc46e0ace36
Prescott <carlsonprescott@gmail.com>
2023-02-02 12:54:58 -0800

    user auth components fixed

--- frontend/components/session_form/signup_form_container.jsx
+++ frontend/components/session_form/signup_form_container.jsx
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import { signup, clearErrors } from '../../actions/session_actions';
+import React from 'react';
 // import SignupForm from './signup_form';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router-dom'
